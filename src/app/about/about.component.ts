3ec7062e5c1a6d83ac987656905bd180c24b702b
uppu540 <upendra.giduturi540@outlook.com>
2023-03-30 14:16:16 +0530

    added changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  type: any = '';
+  type: any = 'general';
   constructor() { }
 
   ngOnInit(): void {
